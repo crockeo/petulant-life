1a912aa66ce69310dba044be744328e99df050bc
Crockeo <crockeo@gmail.com>
2014-12-01 13:54:22 -0500

    Added overtone as a dep

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :main petulant-life.main
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone "0.9.1"]
                  [org.lwjgl.lwjgl/lwjgl "2.9.1"]
                  [org.lwjgl.lwjgl/lwjgl-platform "2.9.1"
                   :classifier "natives-linux"
